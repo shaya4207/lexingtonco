e9be037ada730da9fccee1ce0543312d0ae99396
Shaya Krauss <shaya4207@gmail.com>
2013-01-13 20:37:42 -0500

    some updates

--- admin/add_property.php
+++ admin/add_property.php
@@ -7,10 +7,18 @@
   </head>
   <body>
     <form action="add_property_.php" method="post" enctype="multipart/form-data">
-      <input type="text" name="name" /><br />
-      <input type="text" name="address" /><br />
-      <input type="text" name="city" /><br />
-      <select name="state">
+      <table cellpadding="0" cellspacing="0">
+        <tr>
+          <td align="left">Name</td><td align="left"><input type="text" name="name" /></td>
+        </tr>
+        <tr>
+          <td align="left">Address</td><td align="left"><input type="text" name="address" /></td>
+        </tr>
+        <tr>
+          <td align="left">City</td><td align="left"><input type="text" name="city" /></td>
+        </tr>
+        <tr>
+          <td align="left">State</td><td align="left"><select name="state">
         <option selected disabled value=""></option>
         <?php
           $q = mysql_query("SELECT id,name FROM us_states");
@@ -20,12 +28,22 @@
         <?php
           }
         ?>
-      </select><br />
-      <input type="text" name="zip" /><br />
-      <input type="text" name="lease_contact[]" /><br />
-      <input type="text" name="lease_contact[]" /><br />
-      <input type="text" name="lease_contact[]" /><br />
-      <select name="prop_type">
+      </select></td>
+        </tr>
+        <tr>
+          <td align="left">Zip</td><td align="left"><input type="text" name="zip" /></td>
+        </tr>
+        <tr>
+          <td align="left">Lease Contact</td><td align="left"><input type="text" name="lease_contact[]" /></td>
+        </tr>
+        <tr>
+          <td align="left">Lease Contact</td><td align="left"><input type="text" name="lease_contact[]" /></td>
+        </tr>
+        <tr>
+          <td align="left">Lease Contact</td><td align="left"><input type="text" name="lease_contact[]" /></td>
+        </tr>
+        <tr>
+          <td align="left">Property Type</td><td align="left"><select name="prop_type">
         <option selected disabled value=""></option>
         <?php
           $q = mysql_query("SELECT id,name FROM prop_types");
@@ -35,15 +53,33 @@
         <?php
           }
         ?>
-      </select><br />
-      <input type="text" name="built" /><br />
-      <input type="text" name="renovated" /><br />
-      <input type="text" name="total_sq_ft" /><br />
-      <input type="text" name="avail_space" /><br />
-      <textarea name="description"></textarea><br />
-      <input type="text" name="website" /><br />
-      <input type="file" name="image" /><br />
-      <input type="submit" />
+      </select></td>
+        </tr>
+        <tr>
+          <td align="left">Year Built</td><td align="left"><input type="text" name="built" /></td>
+        </tr>
+        <tr>
+          <td align="left">Year Renovated</td><td align="left"><input type="text" name="renovated" /></td>
+        </tr>
+        <tr>
+          <td align="left">Total Sq. Ft</td><td align="left"><input type="text" name="total_sq_ft" /></td>
+        </tr>
+        <tr>
+          <td align="left">Available space</td><td align="left"><input type="text" name="avail_space" /></td>
+        </tr>
+        <tr>
+          <td align="left">Description</td><td align="left"><textarea name="description"></textarea></td>
+        </tr>
+        <tr>
+          <td align="left">Website</td><td align="left"><input type="text" name="website" /></td>
+        </tr>
+        <tr>
+          <td align="left">Image</td><td align="left"><input type="file" name="image" /></td>
+        </tr>
+        <tr>
+          <td align="right" colspan="2"><input type="submit" /></td>
+        </tr>
+      </table>
     </form>
   </body>
 </html>
--- admin/add_property_.php
+++ admin/add_property_.php
@@ -1,4 +1,16 @@
 <?php
-  echo "<pre>";
-  print_r($_POST);
-  print_r($_FILES);
+  $name = $_POST['name'];
+  $address = $_POST['address'];
+  $city = $_POST['city'];
+  $zip = $_POST['zip'];
+  $lease_contact = $_POST['lease_contact'];
+  $built = $_POST['built'];
+  $renovated = $_POST['renovated'];
+  $total_sq_ft = $_POST['total_sq_ft'];
+  $avail_space = $_POST['avail_space'];
+  $description = $_POST['description'];
+  $website = $_POST['website'];
+  
+  
+  
+  
